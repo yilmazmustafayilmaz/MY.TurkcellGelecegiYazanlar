144447024068e9f0e95521deeb5437569c6bd330
YILMAZ <YILMAZ@DESKTOP-RR24A2S>
2023-03-08 22:13:53 +0300

    TryCatch

---
+++ C#/Basics/TryAndCatch/TryAndCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.WriteLine("Bir sayı giriniz");
+    var sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Bir sayı daha1 giriniz");
+    int sayi2 = Convert.ToInt32(Console.ReadLine());
+    int bolum = sayi1 / sayi2;
+    Console.WriteLine($"Bölme işleminin sonucu: {bolum}");
+
+}
+catch (FormatException)
+{
+    Console.WriteLine("Lan ergen sayı diyoruz!");
+
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Tam sayılar 0'a bölünemez!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Burası mutlaka çalışması gereken bir satırsa? ");
+}
