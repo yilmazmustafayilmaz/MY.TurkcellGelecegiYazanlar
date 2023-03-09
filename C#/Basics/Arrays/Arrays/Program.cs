98a15169acdf7402d597fb8ea9fcde520b6c2d35
YILMAZ <YILMAZ@DESKTOP-RR24A2S>
2023-03-09 21:28:47 +0300

    Array and Loop

---
+++ C#/Basics/Arrays/Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//1. Yol: Kaç elemandan oluşacağını biliyorum ama değerlerini bilmiyorum
+string[] books = new string[8];
+//2. Yol Kaç elemandan oluşacağını da değerlerini de biliyorum
+string[] seasons = new string[] { "İlkbahar", "Yaz", "Sonbahar", "Kış" };
+
+books[0] = "Yüzüklerin Efendisi";
+books[7] = "Otostopçunun Galaksi Rehberi";
+
+Console.WriteLine(seasons[0]);
+Console.WriteLine(seasons[seasons.Length - 1]);
+
+/*
+ * Girilen iki basamaklı bir sayıyı yazıya dönüştürme
+ */
+
+string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+
+Console.WriteLine("İki basamaklı bir sayı giriniz.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int onlarBasamagi = number / 10;
+int birlerBasamagi = number % 10;
+
+Console.WriteLine($"{onlar[onlarBasamagi]} {birler[birlerBasamagi]}");
---
+++ C#/Basics/Lops/Lops/Program.cs
@@ -0,0 +1,63 @@
+﻿//******************************While Loop******************************
+//Sayı bulmaca oyunu
+
+Random randomNumberGenerator = new Random();
+
+int puzzle = randomNumberGenerator.Next(0, 100);
+bool isWin = false;
+
+while (!isWin)
+{
+    Console.WriteLine("Lütfen tahmininizi girini:");
+    int suggest = Convert.ToInt32(Console.ReadLine());
+
+    if (suggest < puzzle)
+        Console.WriteLine("Yukarı!");
+    else if (suggest > puzzle)
+        Console.WriteLine("Aşağı!");
+    else
+        Console.WriteLine("Bildiniz");
+        isWin = true;
+}
+
+//Tüm elemanları yazdır
+int[] numbers = { 13, 2, 7, 9, -4, 3 };
+
+int index = 0;
+while (index < numbers.Length)
+{
+    Console.WriteLine(numbers[index++]);
+}
+
+//******************************For Loop******************************
+int[] numbers2 = { 13, 2, 7, 9, -4, 3 };
+for (int i = 0; i < numbers2.Length; i++)
+{
+    Console.WriteLine(numbers2[i]);
+}
+
+string[] countries = { "Türkiye", "Almanya", "Hollanda", "İsveç", "Kıbrıs" };
+Console.WriteLine("Bir ülke giriniz");
+string country = Console.ReadLine();
+
+bool isFind = false;
+
+for (int i = 0;i < countries.Length; i++)
+{
+    if (countries[i] == country)
+    {
+        isFind = true;
+        break;
+    }
+}
+
+if (isFind)
+{
+    Console.WriteLine("Vardır");
+}
+else
+{
+    Console.WriteLine("Yoktur");
+}
+
+
