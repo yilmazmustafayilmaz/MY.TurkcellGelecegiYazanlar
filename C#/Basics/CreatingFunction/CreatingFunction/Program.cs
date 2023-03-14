5025324b3daa4952c0b921a2439eecb5e4b24406
YILMAZ <YILMAZ@DESKTOP-RR24A2S>
2023-03-14 14:02:13 +0300

    creatingFunction

---
+++ C#/Basics/CreatingFunction/CreatingFunction/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * 1. Bir kelime koleksiyonu içinden rastgele bir kelime SEÇ.
+ * 2. Seçilen kelimeyi, "* * * *" biçiminde bulmacaya ÇEVİR.
+ * 3. Kullanıcıdan harf İSTE.
+ * 4. girilen Harfi Kelimede ARA
+ * 5. Varsa; o yıldızı harfe ÇEVİR
+ * 6. Tüm *'lar açılana kadar bu adımlar devam etsin.
+ */
+
+List<string> words = new List<string>() { "ayna" };
+var choosingWord = getRandomWord(words);
+var puzzledWord = convertToPuzzle(choosingWord);
+showOnScreen(puzzledWord);
+string suggestedLetter = getLetterFromUser();
+
+if (isLetterFindInWord(choosingWord, suggestedLetter))
+{
+    puzzledWord = replaceStarWithLetter(choosingWord, puzzledWord, suggestedLetter);
+    showOnScreen(puzzledWord);
+}
+else
+{
+    showOnScreen("Bir hakkınız yandı...");
+}
+
+string getRandomWord(List<string> words)
+{
+    var index = new Random().Next(words.Count);
+    return words[index];
+}
+
+string convertToPuzzle(string word)
+{
+    string puzzle = string.Empty;
+    for (int i = 0; i < word.Length; i++)
+    {
+        puzzle += "*";
+    }
+    return puzzle;
+
+}
+
+void showOnScreen(string word)
+{
+    Console.WriteLine(word);
+}
+
+string getLetterFromUser()
+{
+    Console.WriteLine("Bir harf giriniz....");
+    return Console.ReadLine();
+}
+
+bool isLetterFindInWord(string word, string letter)
+{
+    return word.ToLower().Contains(letter.ToLower());
+}
+
+string replaceStarWithLetter(string word, string puzzle, string harf)
+{
+    return "a**a";
+}
