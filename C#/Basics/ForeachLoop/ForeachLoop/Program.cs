6ac99e3515c61a602a1301a8b233552a54dd0eae
YILMAZ <YILMAZ@DESKTOP-RR24A2S>
2023-03-14 13:54:23 +0300

    foreachLoop

---
+++ C#/Basics/ForeachLoop/ForeachLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] products = { "A", "B", "C" };
+
+Array.Resize(ref products, products.Length + 1);
+List<string> productList = new List<string> { "TV", "SoundBar", "XBOX One" };
+
+List<string> shoppingCart = new List<string>();
+
+foreach (var product in productList)
+{
+
+    shoppingCart.Add(product);
+    // productList.Remove(product);
+}
+
+foreach (var item in shoppingCart)
+{
+    productList.Remove(item);
+}
